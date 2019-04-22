08123ad510f8f2986013acacdf453ae3e9968a44
zhiyu.c.wang <zhiyu.c.wang@accenture.com>
2019-04-22 14:27:25 +0800

    add apply-call-bind.js

--- javascript/apply-call-bind/apply-call-bind.js
+++ javascript/apply-call-bind/apply-call-bind.js
@@ -17,4 +17,76 @@
     Math.max(null, [17, 33, 2]);
     等价于
     Math.max(17, 33, 2);
+
+    2. func.call(①thisValue, ②param_1, param_2, ..., param_n)
+    ① thisValue
+      当函数执行时 this指向的上下文， 当不需要绑定对象时，参数设置为null
+    ② param11, ..., param_n
+      接受单个参数
+    const testObj = {
+      name: 'shadow',
+      sayWords(words) {
+        console.log(`${words}, ${this.name}`);
+      }
+    };
+    const testObj2 = {
+      name: 'tricker'
+    };
+    testObj.sayWords('hi');   => hi, shadow
+    testObj.sayWords.call(testObj2, 'hello');   => hello, tricker
+    * 最后一行，因为 testObj2上并没有 sayWords 方法，因此按正常无法运行
+    * 但是可以通过调用testObj上的方法来使其执行   => testObj.sayWords.call(testObj2, 'hello');
+    * 也可以在定义testObj2的时候直接修改， 例如：
+    const testObj = {
+      name: 'shadow',
+      sayWords(words) {
+        console.log(`${words}, ${this.name}`);
+      }
+    };
+    const testObj2 = {
+      name: 'tricker',
+      sayWords(words) {
+        // testObj.sayWords(words);
+        testObj.sayWords.call(this, words);
+      }
+    };
+    testObj.sayWords('hi');   => hi, shadow
+    testObj2.sayWords('hi');  => hi, tricker
+    * 此处testObj2 在声明时直接引用 objTest 的 sayWords 方法，
+    * testObj.sayWords.call(this, words), this将在方法执行时绑定到 testObj2 上
+
+    3. func.bind(①thisValue, ②param_1, param_2, ..., param_n)
+    * 返回一个调用func的新函数，并将 params参数按顺序作为func的参数传递
+    ① thisValue
+      当函数执行时 this指向的上下文， 当不需要绑定对象时，参数设置为null
+    ② param11, ..., param_n
+      接受单个参数
+    function add(x, y) {
+      return x + y;
+    }
+    const plus1 = add.bind(null, 1);
+    console.log(plus(5)); => 6
+    * 实际上段代码等同于
+    function add(x, y) {
+      return x + y;
+    }
+    function plus1(num) {
+      return add(1, num)
+    }
+    console.log(plus1(5));
  */
+const testObj = {
+  name: 'shadow',
+  sayWords(words) {
+    console.log(`${words}, ${this.name}`);
+  }
+};
+const testObj2 = {
+  name: 'tricker',
+  sayWords(words) {
+    testObj.sayWords.call(this, words);
+  }
+};
+testObj.sayWords('hi');
+testObj2.sayWords('hi');
+
